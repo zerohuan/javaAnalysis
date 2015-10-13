1821636bbf14c9767a1afd1c5264c66977a0725a
zerohuan <yanjinheng@qq.com>
2015-10-13 13:53:19 +0800

    static inner class test

--- src/main/java/com/yjh/charset/CharAndByte.java
+++ src/main/java/com/yjh/charset/CharAndByte.java
@@ -30,6 +30,21 @@ public class CharAndByte {
         //UTF_8
         System.out.println(PrintUtil.toHex(str.getBytes("utf-8")));
 
+        //My environment is BigEndian
+        System.out.println((0x960e));
+        System.out.println((int)'阎');
+
+
+        //encode and decode
+        String str1 = "淘！我喜欢！";
+        String str1GBK2ISO = new String(str1.getBytes("gbk"), "iso-8859-1");
+        System.out.println(str1GBK2ISO);
+
+        String str1ISO2ISO = new String(str1.getBytes("iso-8859-1"), "iso-8859-1");
+        System.out.println(str1ISO2ISO);
+
+        String str1GBK2ISO_GBK2GBK = new String(new String(str1.getBytes("gbk"), "iso-8859-1").getBytes("gbk"), "gbk");
+        System.out.println(str1GBK2ISO_GBK2GBK);
     }
 
 
---
+++ src/main/java/com/yjh/inner/A.java
@@ -0,0 +1,27 @@
+package com.yjh.inner;
+
+/**
+ * Created by yjh on 15-10-13.
+ */
+public class A {
+    public static int a = 1;
+    static {
+        System.out.println("A initialized");
+    }
+
+    static class B {
+        public static int b = 1;
+        static {
+            System.out.println("B initialized");
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        //初始化A时，并不会初始化A的静态内部类
+        Class.forName("com.yjh.inner.A");
+//        new A();
+        Class<?> bc = B.class; //不会引起初始化
+        int b = B.b; //会引起初始化
+    }
+}
