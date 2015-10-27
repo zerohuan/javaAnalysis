3597a9606cb3bb125cb8732c5b83eeba8302f391
zerohuan <yanjinheng@qq.com>
2015-10-27 15:36:04 +0800

    台阶问题，旋转数组查找，二进制1的个数

---
+++ src/main/java/com/exercise/DoublePow.java
@@ -0,0 +1,12 @@
+package com.exercise;
+
+/**
+ * 给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。
+ *
+ * Created by yjh on 15-10-27.
+ */
+public class DoublePow {
+    public double Power(double base, int exponent) {
+        return 0;
+    }
+}
